a66b69660c7013e5c377c5cc4539fe95fde25781
Rashon Rogers <rashonrogersj@gmail.com>
2023-05-11 17:47:30 +0900

    watch list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.new(bookmark_params)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
